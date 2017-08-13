84aaf471355d036a9ce3a191dd4fc7e6b4c1a052
Ed Brannin <edbrannin@gmail.com>
2017-08-12 21:35:03 -0400

    Got Facebook login to return some info

---
+++ README.markdown
@@ -0,0 +1,12 @@
+Ted says:
+
+> I talked it over with David, and our business rules are about the same as they were for the original contacts list. In brief:
+>
+> 1. Secure authentication.
+> 2. Display all contact information in a table on one page. (This allows for in-browser searching, which is more than good enough for our purposes.)
+> 3. Organize contacts into different groups using tags; display all contact information from a given group in a table on one page.
+> 4. Add, remove, and edit contacts and contact information. (The “Notes” field has been especially helpful.)
+> 5. Sort on different columns, especially last name and zip code. (I implemented this client side using a JavaScript library, which has worked just fine.)
+> 6. Generate mailing labels for a given group in PDF.
+>
+> If you set something like this up in Python and gave me a brief orientation on how it operates, I could do a lot in the way of maintenance and troubleshooting. (I’ve done some work in Python, so I wouldn’t be as out-to-sea as I was on the Ruby contacts list.)
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import pprint
+
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from flask_oauthlib.client import OAuth
+from flask_oauthlib.client import OAuth, OAuthException
 
 app = Flask(__name__)
 # app.config.from_object('yourapplication.default_settings')
@@ -10,6 +12,7 @@ db = SQLAlchemy(app)
 
 oauth = OAuth()
 
+# https://flask-oauthlib.readthedocs.io/en/latest/client.html#facebook-oauth
 facebook = oauth.remote_app('facebook',
     base_url='https://graph.facebook.com/',
     request_token_url=None,
@@ -20,8 +23,42 @@ facebook = oauth.remote_app('facebook',
     request_token_params={'scope': 'email'}
 )
 
+# https://flask-oauthlib.readthedocs.io/en/latest/client.html#signing-in-authorizing
+@facebook.tokengetter
+def get_facebook_token(token=None):
+    return session.get('oauth_token')
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/oauth-authorized')
+def oauth_authorized():
+    next_url = request.args.get('next') or url_for('index')
+    resp = facebook.authorized_response()
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    if isinstance(resp, OAuthException):
+        return 'Access denied: %s' % resp.message
+
+    pprint.pprint(resp)
+    session['oauth_token'] = (resp['access_token'], '')
+    pprint.pprint(session)
+    me = facebook.get('/me')
+    session['me'] = me.data
+
+    print 'Logged in as id={} name={} redirect={}'.format(
+        me.data['id'], me.data['name'], request.args.get('next'))
+    flash('You were signed in as {} ({})'.format(me.data['name'], me.data['id']))
+    return redirect(next_url)
+
 @app.route('/')
 def home():
     return render_template('home.html.j2')
 
-
+app.secret_key = app.config.get('SECRET_KEY', "DEV TESTING")

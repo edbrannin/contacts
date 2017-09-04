bd68509cf79fa90a5f88b4941796a4530e8c623d
Ed Brannin <edbrannin@gmail.com>
2017-09-04 13:50:37 -0400

    Don't complain if SIM_CONTACTS_SETTINGS is not set.

--- contacts/__init__.py
+++ contacts/__init__.py
@@ -3,7 +3,7 @@ from flask_oauthlib.client import OAuth, OAuthException
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('contacts.default_config')
-# app.config.from_envvar('SIM_CONTACTS_SETTINGS')
+app.config.from_envvar('SIM_CONTACTS_SETTINGS', silent=True)
 app.config.from_pyfile('config.py')
 
 

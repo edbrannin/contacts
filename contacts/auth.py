d34784e80804689d13a958724d0a9a8ba135d986
Ed Brannin <edbrannin@gmail.com>
2022-02-11 23:05:32 -0500

    Fix Facebook login

--- contacts/auth.py
+++ contacts/auth.py
@@ -1,6 +1,5 @@
 import traceback
 from functools import wraps
-import pprint
 import hashlib
 import time
 
@@ -13,13 +12,18 @@ oauth = OAuth(app)
 
 # https://flask-oauthlib.readthedocs.io/en/latest/client.html#facebook-oauth
 facebook = oauth.register('facebook',
-    base_url='https://graph.facebook.com/',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=app.config['FACEBOOK_APP_ID'],
-    consumer_secret=app.config['FACEBOOK_APP_SECRET'],
-    request_token_params={'scope': 'email'}
+    # request_token_url=None,
+    client_id=app.config['FACEBOOK_APP_ID'],
+    client_secret=app.config['FACEBOOK_APP_SECRET'],
+    request_token_params={'scope': 'email'},
+    access_token_params={'scope': 'email'},
+
+    api_base_url= 'https://graph.facebook.com/v7.0/',
+    access_token_url= 'https://graph.facebook.com/v7.0/oauth/access_token',
+    authorize_url= 'https://www.facebook.com/v7.0/dialog/oauth',
+    client_kwargs= {'scope': 'email public_profile'},
+    # userinfo_endpoint= USERINFO_ENDPOINT,
+
 )
 facebook = oauth.create_client('facebook')
 
@@ -55,9 +59,7 @@ def login():
         _scheme=scheme,
         _external=True
     )
-    print('callback_url:')
-    print(callback_url)
-    return facebook.authorize_redirect(callback=callback_url)
+    return facebook.authorize_redirect(callback_url)
 
 @app.route('/logout')
 def logout():
@@ -71,7 +73,8 @@ def logout():
 @app.route('/oauth-authorized')
 def oauth_authorized():
     next_url = request.args.get('next') or url_for('index')
-    resp = facebook.authorized_response()
+    resp = facebook.authorize_access_token()
+
     if resp is None:
         flash('You denied the request to sign in.')
         return redirect(next_url)
@@ -79,26 +82,26 @@ def oauth_authorized():
     if isinstance(resp, OAuthError):
         return 'Access denied: %s' % resp.message
 
-    pprint.pprint(resp)
     session['oauth_token'] = (resp['access_token'], '')
-    pprint.pprint(session)
     me = facebook.get('/me/?fields=email,name,id,picture.height(50).width(50),first_name,last_name')
-    g.user = me.data
-    session['me'] = me.data
-    email = me.data['email'].strip().lower()
+
+    user = me.json()
+    email = user['email'].strip().lower()
 
     if email not in app.config['ALLOWED_USERS']:
         flash("You are not one of the allowed users.  Please ask an admin for access.")
         return logout()
 
+    g.user = user
+    session['me'] = user
     try:
-        session['picture_url'] = me.data['picture']['data']['url']
+        session['picture_url'] = user['picture']['data']['url']
     except KeyError:
         session['picture_url'] = "https://www.gravatar.com/avatar/{}".format(hashlib.md5(email).hexdigest())
 
     print('Logged in as id={} name={} redirect={}'.format(
-        me.data['id'], me.data['name'], request.args.get('next')))
-    flash('You were signed in as {} ({})'.format(me.data['name'], me.data['id']))
+        user['id'], user['name'], request.args.get('next')))
+    flash('You were signed in as {} ({})'.format(user['name'], user['id']))
     return redirect(next_url)
 
 def login_required(f):
